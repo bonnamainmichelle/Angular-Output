07ca36567d03e09ec358c3df1a1ad172641577bd
bonnamainmichelle <bonnamainmichelle@gmail.com>
2022-08-26 17:55:17 -0300

    1 file modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { PaiComponent } from './pai/pai.component';
+import { FilhoComponent } from './filho/filho.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, HelloComponent, PaiComponent, FilhoComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
